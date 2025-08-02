f54b4b3beb0bea3031708f878608652b74c45082
qabas3 <qabasalghawi@gmail.com>
2025-08-02 21:53:38 +0400

    Add solution for Logic2-blackjack

---
+++ Logic2/blackjack.java
@@ -0,0 +1,13 @@
+public int blackjack(int a, int b) {
+
+    if (a > 21 && b > 21) {
+        return 0;
+    }
+    if (a > 21) {
+        return b;
+    }
+    if (b > 21) {
+        return a;
+    }
+    return (21 - a) <= (21 - b) ? a : b;
+}
